a0bc888ddf2055752910e421fe09d3046872f5b9
ikramsyawal <ikramsyawalalitu@gmail.com>
2022-09-10 15:01:16 +0800

    final commit v1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 var typed = new Typed(".typed-string", {
     strings: ["Front End Developer", "Python Developer", "3D Artist"],
@@ -5,3 +13,20 @@ var typed = new Typed(".typed-string", {
     backSpeed: 80,
     loop: true
 })
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
